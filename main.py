d8a9229a9d80537fbb6c9485178f96f07e06f8bb
JustineXp <justineislearning@gmail.com>
2024-03-06 17:25:56 +0300

    Added a bonus food Logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake_class import Snake
 from food import Food
+from bonus import Bonus
 from scoreboard import ScoreBoard
 import time
 X_Y_DIMENSION = 600
@@ -15,26 +16,35 @@ screen.listen()
 snake = Snake()
 food = Food()
 score = ScoreBoard()
+bonus = Bonus()
 
 game_is_on = True
+bonus_time = False
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.1)
     snake.turn_snake(screen)
 
     if snake.turtle.distance(food) < 15:
         food.refresh()
         snake.add_extra_piece()
+        if snake.turtle.distance(bonus) < 15:
+            bonus.refresh()
+            snake.add_extra_piece()
+            score.updatescore(5)
         score.updatescore()
+
+    if snake.turtle.distance(bonus) < 15:
+        bonus.refresh()
+        snake.add_extra_piece()
+        score.updatescore(5)
 
     if snake.turtle.xcor() > 280 or snake.turtle.xcor() < -280 or snake.turtle.ycor() > 280 or snake.turtle.ycor() < -280:
         game_is_on = False
         score.end_game()
 
-    for segment in snake.turtles:
-        if segment == snake.turtle:
-            pass
-        elif snake.turtle.distance(segment) < 10:
+    for segment in snake.turtles[1:]:
+        if snake.turtle.distance(segment) < 10:
             game_is_on = False
             score.end_game()
 
